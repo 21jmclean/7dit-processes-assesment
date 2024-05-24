bec602e36a1c588894a163aae8793b0818e84484
21jmclean <21jmclean@wakatipu.school.nz>
2024-05-24 14:14:23 +1200

    Basic HTML page

---
+++ processes_assesment/main.js
@@ -0,0 +1,4 @@
+var input = document.getElementById("dateInput")
+alert("https://history.openweathermap.org/data/2.5/history/city?lat={45.0302}&lon={168.6615}&type=hour&start={12/04/2014}&end={12/04/2014}&appid={1bc6552b4acc4a0f6e62c87566b06d0a}")
+
+
