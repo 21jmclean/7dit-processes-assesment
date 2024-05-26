bc2383f510a21d50484b247faf1d3076418604f1
21jmclean <21jmclean@wakatipu.school.nz>
2024-05-26 18:05:15 +1200

    Modified the code to only output the temperature, not including the square brakets.

--- processes_assesment/main.js
+++ processes_assesment/main.js
@@ -1,5 +1,4 @@
 const apiurl = "https://archive-api.open-meteo.com/v1/archive?latitude=-45.0302&longitude=168.6627&start_date=2024-05-22&end_date=2024-05-22&daily=temperature_2m_max&timezone=Pacific%2FAuckland"
-var temp = 0
 
 fetch(apiurl)
     .then(response => {
@@ -9,11 +8,8 @@ fetch(apiurl)
         return response.json();
     })
     .then(data => {
-         console.log(data.daily.temperature_2m_max)
+         console.log(data.daily.temperature_2m_max[0])
     })
     .catch(error => {
         console.error('Error:', error);
     });
-
-
-console.log(temp)
