97404b533160e8303f0ccc1463da44a85913ec38
21jmclean <21jmclean@wakatipu.school.nz>
2024-05-28 09:59:59 +1200

    Temperature now is logged on the website itself instead of the console.

--- processes_assesment/main.js
+++ processes_assesment/main.js
@@ -1,5 +1,7 @@
 const apiurl = "https://archive-api.open-meteo.com/v1/archive?latitude=-45.0302&longitude=168.6627&start_date=2024-05-25&end_date=2024-05-25&daily=temperature_2m_max&timezone=Pacific%2FAuckland"
 
+let temperature = ""
+
 fetch(apiurl)
     .then(response => {
         if (!response.ok) {
@@ -8,8 +10,15 @@ fetch(apiurl)
         return response.json();
     })
     .then(data => {
-        console.log(data.daily.temperature_2m_max[0])
+        temperature = data.daily.temperature_2m_max[0]
     })
     .catch(error => {
         console.error('Error:', error);
     });
+
+function log_temperature() {
+    let datepick = document.getElementById("dateInput")
+    document.getElementById("date").innerHtml = datepick
+    console.log(temperature)
+    document.getElementById("temperature").innerHTML = temperature
+}
