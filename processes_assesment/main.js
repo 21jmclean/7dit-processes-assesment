84da7af3c28ce1075442ff349a8364e145450f9c
21jmclean <21jmclean@wakatipu.school.nz>
2024-05-29 17:06:40 +1200

    Program can now have date inputted, and a temperature will show up for that date. There is one error where the button has to be pressed twice, in order to get the temperature.

--- processes_assesment/main.js
+++ processes_assesment/main.js
@@ -1,24 +1,31 @@
-const apiurl = "https://archive-api.open-meteo.com/v1/archive?latitude=-45.0302&longitude=168.6627&start_date=2024-05-25&end_date=2024-05-25&daily=temperature_2m_max&timezone=Pacific%2FAuckland"
-
 let temperature = ""
+let datepick = ""
 
-fetch(apiurl)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(data => {
-        temperature = data.daily.temperature_2m_max[0]
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-
-function log_temperature() {
-    let datepick = document.getElementById("dateInput")
-    document.getElementById("date").innerHtml = datepick
-    console.log(temperature)
-    document.getElementById("temperature").innerHTML = temperature
+function get_date() {
+    let datepick = document.getElementById("dateInput").value;
+    document.getElementById("date").textContent = datepick;
+    get_weather()
 }
+
+function get_weather() {
+    let date = datepick
+    const apiUrl = `https://archive-api.open-meteo.com/v1/archive?latitude=-45.0302&longitude=168.6627&start_date=${date}&end_date=${date}&daily=temperature_2m_max&timezone=Pacific%2FAuckland`
+
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            temperature = data.daily.temperature_2m_max[0]
+
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+        
+        console.log(temperature)
+        document.getElementById("temperature").innerHTML = temperature + "°";
+    }
