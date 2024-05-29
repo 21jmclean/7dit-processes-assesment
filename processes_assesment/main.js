9aba003774ae1b3626cfa91607ff7141d2c43e85
21jmclean <21jmclean@wakatipu.school.nz>
2024-05-29 21:12:40 +1200

    Date and temperatue now both updat at once.

--- processes_assesment/main.js
+++ processes_assesment/main.js
@@ -1,18 +1,10 @@
-let temperature = ""
-let datepick = ""
-let date = ""
-
-function get_date() {
+async function get_weather() {
     let datepick = document.getElementById("dateInput").value;
-    document.getElementById("date").textContent = datepick;
-    date = datepick
-    get_weather()
-}
+    date = datepick;
 
-function get_weather() {
     const apiUrl = `https://archive-api.open-meteo.com/v1/archive?latitude=-45.0302&longitude=168.6627&start_date=${date}&end_date=${date}&daily=temperature_2m_max&timezone=Pacific%2FAuckland`
 
-    fetch(apiUrl)
+    await fetch(apiUrl)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
@@ -28,4 +20,5 @@ function get_weather() {
         });
         
         document.getElementById("temperature").innerHTML = temperature + "°";
+        document.getElementById("date").textContent = datepick
     }
